f258631d4ade81e1547a231fe92152914e36568d
wangxiaomin <wangxiaomin@800617.com>
2017-07-12 08:52:23 +0800

    modify project name

--- devops/wsgi.py
+++ devops/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devops.settings")
 
 application = get_wsgi_application()
