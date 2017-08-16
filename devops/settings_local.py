745d5c66e10c271bf74569f1abfd71c68d762d9f
wangxiaomin <wangxiaomin@800617.com>
2017-08-16 18:08:18 +0800

    modify netapi

--- devops/settings_local.py
+++ devops/settings_local.py
@@ -1,4 +1,4 @@
-SALT_API = {"url": "https://localhost:9090",
-            "user": "sa",
-            "password": "qitan"
+SALT_API = {"url": "https://devops.ctu.com:8000",
+            "user": "saltapi",
+            "password": "Ctu800617Ctu"
             }
