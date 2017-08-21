adea152b2537a7c5ee12379353236c0030236be1
wangxiaomin <15801006614@163.com>
2017-08-21 14:59:55 +0800

    db: sqlite -> mysql

--- devops/settings.py
+++ devops/settings.py
@@ -89,14 +89,14 @@ WSGI_APPLICATION = 'devops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       # 'ENGINE': 'django.db.backends.mysql',
-       # 'NAME': 'devopsdb',
-       # 'HOST': 'test.devops.cn',
-       # 'USER': 'root',
-       # 'PORT': 3306,
-       # 'PASSWORD': 'ctu800617',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'devopsdb',
+       'HOST': 'test.devops.cn',
+       'USER': 'root',
+       'PORT': 3306,
+       'PASSWORD': 'ctu800617',
     }
 }
 
