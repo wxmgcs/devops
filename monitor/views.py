ca7023d4cb8b19bf35c4c5ea661433f85d7d8554
wangxiaomin <15801006614@163.com>
2017-08-24 10:28:03 +0800

    添加 vpn_code

--- monitor/views.py
+++ monitor/views.py
@@ -6,9 +6,8 @@ from .models import  UnicomRechargeGenOrder
 
 def upload_genorder(request):
     if request.method == 'POST':
-        print request.POST
-        print request.POST
         data = json.loads(request.POST['genorder'])
+        vpn_code = request.POST['vpn_code']
         keys = data.keys()
         for key in keys:
             vals = data.get(key)
@@ -16,7 +15,7 @@ def upload_genorder(request):
                 print (item['status'],item['create_time'])
                 status = item['status']
                 create_time = item['create_time']
-                UnicomRechargeGenOrder.objects.create(eid=key,status=status,create_time=create_time)
+                UnicomRechargeGenOrder.objects.create(eid=key,status=status,create_time=create_time,vpn_code=vpn_code)
         return HttpResponse(json.dumps(dict(result=0)))
     elif request.method == 'GET':
         return HttpResponse("ok")
