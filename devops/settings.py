f1d83143fe7dbcd50f1c912b8f3d93716cdb88b9
wangxiaomin <15801006614@163.com>
2017-08-24 09:54:44 +0800

    修改库名

--- devops/settings.py
+++ devops/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
        # 'ENGINE': 'django.db.backends.mysql',
-       # 'NAME': 'devopsdb',
+       # 'NAME': 'ctu_devops',
        # 'HOST': 'test.devops.cn',
        # 'USER': 'root',
        # 'PORT': 3306,
