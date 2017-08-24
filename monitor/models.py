6a680b3786f4faaa0b3f467a66b9310715864e4e
wangxiaomin <15801006614@163.com>
2017-08-24 10:20:51 +0800

    monitor genorder add field:vpn_code

--- monitor/models.py
+++ monitor/models.py
@@ -8,6 +8,7 @@ class UnicomRechargeGenOrder(models.Model):
     eid = models.CharField(max_length=255, default='', blank=True, null=True, verbose_name=u'程序编号',
                                   error_messages={'required': u'程序名不能为空'})
     status = models.IntegerField(default=0, blank=True, null=True, verbose_name=u'下单状态')
+    vpn_code = models.IntegerField(default=0, blank=True, null=True, verbose_name=u'vpn编号')
     create_time = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
